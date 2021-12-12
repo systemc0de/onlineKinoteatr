a38bb0ff7a5f51a7ad1189ec044284512718d995
rationalcode <marat_pochta@mail.ru>
2021-12-13 02:43:44 +0500

    6. modify build files

--- app/src/main/java/com/example/onlinekinoteatr/MainActivity.kt
+++ app/src/main/java/com/example/onlinekinoteatr/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.onlinekinoteatr
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.onlinekinoteatr.features.movie_screen.list.ui.MoviesListFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        setFragment(MoviesListFragment())
+    }
+
+    private fun setFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(android.R.id.content, fragment).commit()
     }
 }
--- app/src/main/java/com/example/onlinekinoteatr/features/movie_screen/list/domain/model/MoviesDomainModel.kt
+++ app/src/main/java/com/example/onlinekinoteatr/features/movie_screen/list/domain/model/MoviesDomainModel.kt
@@ -1,7 +1,7 @@
 package com.example.onlinekinoteatr.features.movie_screen.list.domain.model
 
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class MoviesDomainModel(
